0433ee51f23915a433d22b0463efd2411ea62fd6
Sah Yuh <samyun23@bergen.org>
2021-11-03 15:58:28 -0400

    website upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Program is starting!");
+alert('Hello World!');
+//Greet the user
+let message = "Hello User!";
+alert (message);
+let name = prompt("Actually, what is your name?");
+let newMessage = "Greetings, " + name;
+alert(newMessage);
+console.log("Program is ending!");
